512e1a52f0ca5a333f1efeb2c562ac2d5c288b1a
Abhishek0020 <90542072+Abhishek0020@users.noreply.github.com>
2023-04-15 21:22:25 +0530

    Add files via upload

---
+++ Stack.java
@@ -0,0 +1,27 @@
+//package stack;
+//public class Stack {
+//    int arr[];
+//    int capacity;
+//    int top;
+//    Stack(int capacity) {
+//        this.capacity = capacity;
+//        arr = new int[capacity];
+//        top = -1;
+//    }
+//    public void push(int data) {
+//        if (top == capacity - 1) {
+//            System.out.println("Stack is full");
+//            return;
+//        }
+//        arr[top] = data;
+//        public void printStack()
+//        {
+//            for (int i = top; i >= 0; i--) {
+//                System.out.println(arr[i] + "<----");
+//            }
+//            System.out.println();
+//        }
+//
+//
+//    }
+//}
